c82505cb62264f3ae8b016fa61f037635f580a5d
yaswho <noyasmim17@gmail.com>
2022-06-16 12:46:07 -0300

    2

---
+++ src/main/java/inatel/br/cdg/Funcoes/Funcoes.java
@@ -0,0 +1,17 @@
+package inatel.br.cdg.Funcoes;
+
+public class Funcoes {
+
+    //Retorna numero aleatório
+    public int numAleatorio()
+    {
+        int min = 1;
+        int max = 100;
+
+        java.util.Random random = new java.util.Random();
+
+        int value = random.nextInt(max + min) + min;
+
+        return value;
+    }
+}
